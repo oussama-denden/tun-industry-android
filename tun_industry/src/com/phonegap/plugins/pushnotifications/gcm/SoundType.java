32d92fa58585f5b0c700cd9e6a94d37d011cf35e
oussama denden <denden.oussama@gmail.com>
2013-04-20 17:36:06 +0100

    "finished the search feature next the favorite feature"

--- tun_industry/src/com/phonegap/plugins/pushnotifications/gcm/SoundType.java
+++
@@ -1,6 +0,0 @@
-package com.phonegap.plugins.pushnotifications.gcm;
-
-public enum SoundType
-{
-    NO_SOUND, DEFAULT_MODE, ALWAYS
-}
--- tun_industry/src/com/phonegap/plugins/pushnotifications/gcm/VibrateType.java
+++
@@ -1,6 +0,0 @@
-package com.phonegap.plugins.pushnotifications.gcm;
-
-public enum VibrateType
-{
-    NO_VIBRATE, DEFAULT_MODE, ALWAYS
-}
